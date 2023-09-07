07f3027be047d159afaa8db43c3611b81fb9b2bb
selenacdeng <selenacdeng@gmail.com>
2023-09-07 18:27:56 +0800

    Update Figure1.m

--- Figure1.m
+++ Figure1.m
@@ -1,19 +1,19 @@
 %pulling data from excel file for m = 0g
-rawTable = readtable('Figure 1.xlsx','Sheet','Figure 1 (0g)'); 
+data = readtable('Figure 1.xlsx','Sheet','Figure 1 (0g)'); 
 %time for 0g data
-x1 = rawTable.t1;
-x4 = rawTable.t4;
-x7 = rawTable.t7;
+x1 = data.t1;
+x4 = data.t4;
+x7 = data.t7;
 
 %plot a
-y1a = rawTable.pos1;
-y4a = rawTable.pos4;
-y7a = rawTable.pos7;
+y1a = data.pos1;
+y4a = data.pos4;
+y7a = data.pos7;
 
 %plot b
-y1b = rawTable.vel1;
-y4b = rawTable.vel4;
-y7b = rawTable.vel7;
+y1b = data.vel1;
+y4b = data.vel4;
+y7b = data.vel7;
 
 tiledlayout(3,2) %creates plot matrix 'tiles'
 sz = 10; %scatterplot marker size
